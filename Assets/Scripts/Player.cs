b5351c22816f2eb43250042c89d42f04e16c30d9
Jaxson Laramie <jaxsonlaramie@mail.weber.edu>
2024-03-02 01:37:13 -0700

    Added rigid body and colider components

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Ship parameters")]
+    [SerializeField] private float shipAcceleration = 10f;
+    [SerializeField] private float shipMaxVelocity = 10f;
+    [SerializeField] private float shipRotationSpeed = 180f;
+
+    private Rigidbody2D shipRigidbody;
+    private bool isAlive = true;
+    private bool isAccelerating = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        shipRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isAlive) {
+            HandleShipAcceleration();
+            HandleShipRotation();
+        }
     }
+
+    private void FixedUpdate() {
+        if (isAlive && isAccelerating) {
+            shipRigidbody.AddForce(shipAcceleration * transform.up);
+            shipRigidbody.velocity = Vector2.ClampMagnitude(shipRigidbody.velocity, shipMaxVelocity);
+        }
+    }
+
+    private void HandleShipAcceleration() {
+        isAccelerating = Input.GetKey(KeyCode.UpArrow);
+    }
+
+    private void HandleShipRotation() {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            transform.Rotate(shipRotationSpeed * Time.deltaTime * transform.forward);
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.Rotate(-shipRotationSpeed * Time.deltaTime * transform.forward);
+        }
+    }
+
 }
